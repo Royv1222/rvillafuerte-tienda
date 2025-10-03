600789413918ef4d4fc74b51ddf71e6f633f69ad
Royv1222 <rmv2098@gmail.com>
2025-10-02 20:54:39 -0600

    Update leccion 3

---
+++ rvillafuerte-tienda/src/main/java/com/tienda/tienda/services/CategoriaServices.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.tienda.services;
+
+import com.tienda.tienda.domain.Categoria;
+import com.tienda.tienda.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Service
+public class CategoriaServices {
+ 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        }
+        return categoriaRepository.findAll();
+    }
+ 
+ 
+}
