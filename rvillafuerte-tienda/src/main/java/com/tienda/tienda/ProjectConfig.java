c1bfc0d678987a80f5083e68c525f78ac0243d92
Royv1222 <rmv2098@gmail.com>
2025-10-09 21:07:11 -0600

    Leccion 4 FIREBASE

--- rvillafuerte-tienda/src/main/java/com/tienda/tienda/ProjectConfig.java
+++ rvillafuerte-tienda/src/main/java/com/tienda/tienda/ProjectConfig.java
@@ -1,5 +1,15 @@
 package com.tienda.tienda;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -68,6 +78,20 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+   @Value("${firebase.json.path}")
+    private String jsonPath;
+   
+    @Value("${firebase.json.file}")
+    private String jsonFile;
+    
+    @Bean
+    public Storage storage() throws IOException {
+        ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
+        try (InputStream inputStream = resource.getInputStream()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        }
     }
 
 }
